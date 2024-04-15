813292cf1a166e23bfdca52fa224dc85c7d2352f
tziolkowskizseeim <tziolkowski_211i1@e.zseeim.edu.pl>
2024-04-15 09:20:12 +0000

    studentmachine send

---
+++ python/test/main.py
@@ -0,0 +1,13 @@
+
+the_world_is_flat = True
+if the_world_is_flat == True:
+	print ("nie spadnij")
+else :
+	print ("kularzu")
+x = 5
+y = input("podaj y")
+print ("uruchomić")
+a =int(input("podaj a"))
+b =int (input("podaj b"))
+c = a/b
+print (c)
