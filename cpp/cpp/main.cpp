7b53f34e0713b1871e97133388b9336a9c20bc90
tziolkowskizseeim <student@student.machine>
2024-10-28 12:11:16 +0000

    studentmachine send

--- cpp/cpp/main.cpp
+++ cpp/cpp/main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main(){
 	int sr, sc, x, y;
 	int pl[3][3]={0};
-	cout<<"ile razy chcesz zagrac"<<endl;
-	for(int i=0; i<=1; i++){
+	//cout<<"ile razy chcesz zagrac"<<endl;
+	for(int i=0; i<1; i++){
 		cout<<"gracz podaje statek"<<i+1<<"(x,y)"<<endl;
 		cin>>sr;
 		cin>>sc;
